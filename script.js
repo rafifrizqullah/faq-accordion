0db52f5163a875ad35e59cd2ab9d4c188c6027f0
rafifrizqullah <rafifrizqullah@users.noreply.github.com>
2024-02-15 18:00:27 +0700

    Minor update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const acc = document.getElementsByClassName("accordion-button");
 for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
-    const panel = this.parentElement.nextElementSibling;
+    const panel = this.nextElementSibling;
     panel.style.display === "block"
       ? (panel.style.display = "none")
       : (panel.style.display = "block");
