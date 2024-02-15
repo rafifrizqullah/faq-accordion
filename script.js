82512ad3cd7d39f4b388eebabac66ccf1231b7b5
rafifrizqullah <rafifrizqullah@users.noreply.github.com>
2024-02-15 16:59:54 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const acc = document.getElementsByClassName("accordion-button");
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    const panel = this.nextElementSibling;
+    panel.style.display === "block"
+      ? (panel.style.display = "none")
+      : (panel.style.display = "block");
+  });
+}
