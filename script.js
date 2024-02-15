bad2ad4a96c35d6258aa6f166efd543a8364bff3
rafifrizqullah <rafifrizqullah@users.noreply.github.com>
2024-02-15 18:54:26 +0700

    Update accordion

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const acc = document.getElementsByClassName("accordion-button");
+const body = document.getElementsByClassName("accordion-body");
+
+const handleAccordionClick = function () {
+  for (let j = 0; j < body.length; j++) {
+    acc[j].classList.remove("active");
+    body[j].classList.remove("show");
+  }
+  this.classList.add("active");
+  this.nextElementSibling.classList.add("show");
+};
 
 for (let i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    const panel = this.nextElementSibling;
-    panel.style.display === "block"
-      ? (panel.style.display = "none")
-      : (panel.style.display = "block");
-  });
+  acc[0].classList.add("active");
+  acc[0].nextElementSibling.classList.add("show");
+  acc[i].onclick = handleAccordionClick;
 }
